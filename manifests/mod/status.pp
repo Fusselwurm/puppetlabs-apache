d33079f6781067304b78e91ce2e5af3b34e7ed2c
Moritz Schmidt <fusselwurm@gmail.com>
2014-04-16 20:27:36 +0200

    bugfixing: mod_status & adding debian jessie to 2.4 versions

--- manifests/mod/status.pp
+++ manifests/mod/status.pp
@@ -26,6 +26,7 @@
 class apache::mod::status (
   $allow_from      = ['127.0.0.1','::1'],
   $extended_status = 'On',
+  $apache_version = $apache::apache_version,
 ){
   validate_array($allow_from)
   validate_re(downcase($extended_status), '^(on|off)$', "${extended_status} is not supported for extended_status.  Allowed values are 'On' and 'Off'.")
